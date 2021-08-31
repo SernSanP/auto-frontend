ad162298d3cede9112be368409e7b42fe53258d5
Sansern Krachangpis <sansern@findx.co.th>
2021-08-31 09:59:29 +0700

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,27 @@
 import Papa from "papaparse";
+import { useState } from "react";
 
 export default function Home() {
-  var data;
-  Papa.parse('test.csv', {
-    delimiter: "",
-  header: true,
-  dynamicTyping: true,
-  complete: function(results:any) {
-    console.log(results);
-    data = results.data;
-  }
-});
+  const [selectedFile, setSelectedFile] = useState<File>();
+//   var data;
+//   Papa.parse(selectedFile.files[0], {
+//   complete: function(results:any) {
+//     console.log(results);
+//     data = results.data;
+//   }
+// });
+	const changeHandler = (event:any) => {
+		setSelectedFile(event.target.files[0]);
+    Papa.parse(event.target.files[0], {
+      complete: function(results:any) {
+        console.log(results);
+      }
+    });
+	};
   return (
     <div className='p-4'>
       <h1>Hello</h1>
+      <input type="file" name="file" onChange={changeHandler} />
       <div className='text-red-600'>Welcome!</div>
     </div>
   );
